00966a68a5a0df18ac4bebbd58dc4bc19bb72aee
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-11-17 09:34:20 -0800

    Update blockchain_miner_wallet.py

--- unit test/discrete_mining_unit_test/blockchain_miner_wallet/blockchain_miner_wallet.py
+++ unit test/discrete_mining_unit_test/blockchain_miner_wallet/blockchain_miner_wallet.py
@@ -23,20 +23,35 @@ with open("device_a_pub_key.pem") as f:
 #initialize new blockchain(in network terms simulated here as a in-script only database)
 blockchain_list_of_txhashes = []
 blockchain_list_of_txinfo = []
+blockchain_list_of_index = 0;
 
 #mine first block
-difficulty = 1;
-genesis_message = "genesisblock_"+str(difficulty)+"_"+str(time.time())+"_"+device_a_public_key_as_string
-nonce=random.SystemRandom().randint(1, 9223372036854775807)
-block_message = genesis_message+"_"+str(nonce)
-print(block_message)
+difficulty = 3;
 character = "0"
 keep_finding_genesisblock = 1
 while keep_finding_genesisblock == 1:
+	genesis_message = "genesisblock_"+str(difficulty)+"_"+str(time.time())+"_"+device_a_public_key_as_string
+	nonce=random.SystemRandom().randint(1, 9223372036854775807)
+	block_message = genesis_message+"_"+str(nonce)
+	#print(block_message)
 	m = hashlib.sha256()
 	m.update(bytes(block_message, 'utf-8'))
 	digest = m.hexdigest()
-	if digest[0] == character[0]:
-		print(digest)
-		keep_finding_genesisblock = 0;
+	atleast_difficulty_found = 1;
+	difficulty_check_index = 0
+	#print(digest)
+	while difficulty_check_index < difficulty:
+		if digest[difficulty_check_index] != character[0]:
+			#stop while loop(no need to keep searching)
+			difficulty_check_index = difficulty
+			atleast_difficulty_found = 0
+		else:
+			difficulty_check_index += 1
 	
+	#difficulty is atleast satasfactory
+	if atleast_difficulty_found == 1:
+		keep_finding_genesisblock = 0;
+		blockchain_list_of_txhashes[blockchain_list_of_index] = digest
+		blockchain_list_of_txinfo[blockchain_list_of_index] = blockmessage
+		blockchain_list_of_index += 1
+		
