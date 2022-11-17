4d00bf3d113cd134619d1db201b4f635c8fe63b7
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-11-16 16:40:43 -0800

    Update blockchain_miner_wallet.py

--- unit test/discrete_mining_unit_test/blockchain_miner_wallet/blockchain_miner_wallet.py
+++ unit test/discrete_mining_unit_test/blockchain_miner_wallet/blockchain_miner_wallet.py
@@ -12,11 +12,13 @@ device_a_public_key_as_string = ""
 print(device_a_public_key.to_pem())
 
 ##convert to string; store then open as file to convert to string
+device_a_public_key_as_string = ""
+
 with open("device_a_pub_key.pem", "wb") as f:
 	f.write(device_a_public_key.to_pem())
 with open("device_a_pub_key.pem") as f:
 	device_a_public_key_as_string = f.read()
-	print(device_a_public_key_as_string)
+
 	
 #initialize new blockchain(in network terms simulated here as a in-script only database)
 blockchain_list_of_txhashes = []
@@ -24,12 +26,17 @@ blockchain_list_of_txinfo = []
 
 #mine first block
 difficulty = 1;
-genesis_message = "genesisblock_"+str(difficulty)+"_"+str(time.time())+"_"#device_a_public_key.to_pem()
+genesis_message = "genesisblock_"+str(difficulty)+"_"+str(time.time())+"_"+device_a_public_key_as_string
 nonce=random.SystemRandom().randint(1, 9223372036854775807)
 block_message = genesis_message+"_"+str(nonce)
 print(block_message)
+character = "0"
 keep_finding_genesisblock = 1
 while keep_finding_genesisblock == 1:
 	m = hashlib.sha256()
 	m.update(bytes(block_message, 'utf-8'))
-	print(m.hexdigest())
+	digest = m.hexdigest()
+	if digest[0] == character[0]:
+		print(digest)
+		keep_finding_genesisblock = 0;
+	
