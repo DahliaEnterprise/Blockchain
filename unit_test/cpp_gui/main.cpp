9f0f9aeb23fb8cdca08596312b97431620ec2654
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-11-25 04:53:01 +0000

    Create main.cpp

---
+++ unit_test/cpp_gui/main.cpp
@@ -0,0 +1,17 @@
+#include "blockchain.h"
+#include "universal_window.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    blockchain * block_chain = new blockchain();
+    block_chain->initialize();
+    universal_window * window = new universal_window();
+    window->initialize();
+    window->show();
+
+    return a.exec();
+}
