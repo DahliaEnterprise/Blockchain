116607f95c74b22cea74a6cbf4379eb57f28266f
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-12-01 20:54:04 +0000

    Update blockchain.h

--- unit_test/cpp_gui/blockchain/blockchain.h
+++ unit_test/cpp_gui/blockchain/blockchain.h
@@ -1,11 +1,29 @@
-#include "blockchain.h"
+##ifndef BLOCKCHAIN_H
+#define BLOCKCHAIN_H
 
-blockchain::blockchain(QObject *parent) : QObject(parent)
+#include <QObject>
+#include <QJsonObject>
+#include <QJsonDocument>
+#include <QDebug>
+#include <QCryptographicHash>
+#include <QByteArray>
+#include <QRandomGenerator>
+class blockchain : public QObject
 {
+    Q_OBJECT
+public:
+    explicit blockchain(QObject *parent = nullptr);
+    void initialize(uint generate_initial_blockchain);
 
-}
+private:
+    uint difficulty(QByteArray hash);
 
-void blockchain::initialize()
-{
+    QList<QByteArray> hash_list;
+    QList<QByteArray> message_list;
 
-}
+
+signals:
+
+};
+
+#endif // BLOCKCHAIN_H
