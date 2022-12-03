d5472b60c66ab718fe003ba0aa2756d44c7794fc
DahliaEnterprise <35324244+DahliaEnterprise@users.noreply.github.com>
2022-12-03 19:15:14 +0000

    Update main.cpp

--- unit_test/cpp_gui/blockchain/main.cpp
+++ unit_test/cpp_gui/blockchain/main.cpp
@@ -7,8 +7,12 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    uint generate_initial_blockchain = 1;
+    uint blockchain_server = 1;
+    uint search = 1;
+
     blockchain * block_chain = new blockchain();
-    block_chain->initialize();
+    block_chain->initialize(generate_initial_blockchain, blockchain_server, search);
     universal_window * window = new universal_window();
     window->initialize();
     window->show();
